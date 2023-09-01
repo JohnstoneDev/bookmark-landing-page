2627d72e1158c916ef4e21b4a469ed1fd22af0aa
Marucha <marucha.webdev@yahoo.com>
2023-09-01 18:13:23 +0300

    feat: Home Component & Page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { Paths } from './Routes/Routes';
 function App() {
   return (
     <div
-      className="App font-text text-2xl min-h-screen min-w-screen justify-center items-center
-      bg-gradient-to-bl from-slate-300 via-gray-400 to-gray-500 flex flex-col text-amber-800">
+      className="bg-whitish min-h-screen min-w-screen font-global text-regular">
       <Paths />
     </div>
   );
---
+++ src/Pages/HomePage/Home.js
@@ -0,0 +1,9 @@
+import { HomeComponent } from "../../Components/HomeComponent/HomeComponent"
+
+export const Home = () => {
+	return (
+		<>
+			<HomeComponent />
+		</>
+	)
+}
