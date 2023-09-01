06f054280423a82657e608bf4d9f046f49446c2d
Marucha <marucha.webdev@yahoo.com>
2023-09-01 18:10:59 +0300

    style: Theme Configuration: Colors, font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,43 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ],  theme: {
+  ],
+  theme: {
     fontFamily : {
-      main : ['Work Sans', 'sans-serif'],
-      headings : ['Archivo', 'sans-serif'],
-      text : ['Ranade', 'sans-serif']
+      global : ['Rubik', 'sans-serif'],
+    },
+    fontSize : {
+      'heading' : ['36px', {
+            lineHeight : '40px',
+            letterSpacing : '0px',
+            fontWeight : '800',
+          }
+        ],
+      'section' : ['24px', {
+          lineHeight : '32px',
+          letterSpacing : '0px',
+          fontWeight : '600',
+        }
+      ],
+    'button' : ['20px', {
+          lineHeight : '28px',
+          letterSpacing : '0px',
+          fontWeight : '500',
+        }
+      ],
+      'regular' : ['18px', {
+          lineHeight : '28px',
+          letterSpacing : '0px',
+          fontWeight : '400',
+        }
+      ],
+    },
+    colors : {
+      'whitish' : '#f5f5f5',
+      'soft-blue' : 'hsl(231, 69%, 60%)',
+      'soft-red' : 'hsl(0, 94%, 66%)',
+      'grayish-blue' : 'hsl(229, 8%, 60%)',
+      'very-dark-blue' : 'hsl(229, 31%, 21%)',
     },
     screens: {
       'mobile' : '375px',
@@ -14,7 +46,8 @@ module.exports = {
       'laptop': '1024px',
       'desktop': '1280px',
     },
-    extend: {},
+    extend: {
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
